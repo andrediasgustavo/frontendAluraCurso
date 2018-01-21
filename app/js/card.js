9eddb19cb3818d98e6c4a716c9ca53c9ba7f5519
Andre Dias <andre.dias@kanamobi.com.br>
2018-01-21 11:05:15 -0200

    Add delete functionality and alter color when click the colors button

--- app/js/card.js
+++ app/js/card.js
@@ -1,8 +1,24 @@
-var $cardColors = document.querySelectorAll('.card-colors');
 
-for (var i = 0; i < $cardColors.length; i++) {
-    $cardColors[i].addEventListener('click', function() {
-        var $card = this.parentNode.parentNode.parentNode;
-        $card.dataset.color = this.dataset.color;
-    });
-};
+var $cards = document.querySelectorAll('.card');
+var $cardColors = document.querySelectorAll('.card-options');
+
+for (var i = 0; i < $cards.length; i++) {
+  $cards[i].addEventListener('click', function() {
+    var $this = event.target;
+    var $card = this;
+
+    if ($this.dataset.color) {
+      $card.dataset.color = $this.dataset.color;
+
+      for (var i = 0; i < $cardColors.length; i++) {
+        $cardColors[i].classList.remove('isActive')
+      }
+
+      $this.classList.add('isActive');
+    };
+
+    if ($this.classList.contains('card_delete')) {
+      $card.remove()
+    };
+  });
+}
